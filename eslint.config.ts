1e011ce4a8d026a36bf421bbb10db0de08d5c850
Elegant <63347504+dushenyan@users.noreply.github.com>
2025-12-12 14:54:50 +0800

    feat: 优化 ESLint 配置，移除 node_modules 忽略项，调整格式

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,13 +2,14 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   type: 'lib',
+
   vue: true,
+
   typescript: true,
 
   jsonc: true,
 
   ignores: [
-    'node_modules',
     'docs',
   ],
 }, {
